d40891d18df4b5bfd6158ead3a412986630750ed
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2023-08-02 15:59:40 -0600

    Poligono2

--- main.cpp
+++ main.cpp
@@ -36,11 +36,10 @@ void render() {
     clear();
 
     Color polygonColor(255, 255, 255);
-    Color fillColor(255, 255, 0);
+    Color fillColor(0, 0, 255);
 
     std::vector<Vertex2> polygonVertices = {
-            {165, 380}, {185, 360}, {180, 330}, {207, 345}, {233, 330},
-            {230, 360}, {250, 380}, {220, 385}, {205, 410}, {193, 383}
+            {321, 335}, {288, 286}, {339, 251}, {374, 302}
     };
 
     // Dibujar el polígono sin relleno
